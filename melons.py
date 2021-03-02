b4bc51853a4c960b1b9afd954aec10f680cc01d3
Missy Davies <ms.melissadavies@gmail.com>
2021-03-02 06:51:07 +0000

    Edited melons file to be more concise with one dicitonary of melons and edited melon info file to have a more succinct function to print melon data

--- melons.py
+++ melons.py
@@ -1,23 +1,37 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
+melons = {
+    'Honeydew': {
+        'price': 0.99,
+        'seedlessness': True,
+        'flesh color': None,
+        'rind color': None,
+        'weight': None,
+    },
+    'Crenshaw': {
+        'price': 2.00,
+        'seedlessness': False,
+        'flesh color': None,
+        'rind color': None,
+        'weight': None,
+    },
+    'Crane': {
+        'price': 2.50,
+        'seedlessness': False,
+        'flesh color': None,
+        'rind color': None,
+        'weight': None,
+    },
+    'Casaba': {
+        'price': 2.50,
+        'seedlessness': False,
+        'flesh color': None,
+        'rind color': None,
+        'weight': None,
+    },
+    'Cantaloupe': {
+        'price': 0.99,
+        'seedlessness': False,
+        'flesh color': None,
+        'rind color': None,
+        'weight': None,
+    },
+    }
